e6f78d1980f89ec958991057e525970e76f20a25
snarly <snarly@users.noreply.github.com>
2014-11-06 17:12:42 +0100

    Update yt6.js

--- yt6.js
+++ yt6.js
@@ -204,7 +204,7 @@ var proxies = ['https://allow-any-origin.appspot.com/https:','https://cors-anywh
     return [mch,zzz]
   }
 
-  eval(rpt.match(fcobj()[0])[0].replace(fcobj()[1],"dekrypt0") + rpt.match(fs)[0].split(fcobj()[1]).join("dekrypt0"));
+  eval(rpt.match(fcobj()[0])[0].replace(fcobj()[1],"dekrypt0") + ";" + rpt.match(fs)[0].split(fcobj()[1]).join("dekrypt0"));
 
 function dc(sg) {
   return eval(fcnm + '("' + sg + '")');
