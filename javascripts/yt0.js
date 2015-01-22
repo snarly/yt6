a16e3515502b41558eeb23871b72317144e4abb1
snarly <snarly@users.noreply.github.com>
2015-01-22 11:39:42 +0100

    Update yt0.js

--- javascripts/yt0.js
+++ javascripts/yt0.js
@@ -186,18 +186,18 @@ var proxies = ['https://allow-any-origin.appspot.com/https:','https://cors-anywh
       nw = nw.replace('%s', arguments[++i])
     return nw;
   }
-  var fs = new RegExp(    sprintf('function %s[^}]+}[^}]+}', fcnm.replace('$', '\\$'))  );alert("fs="+fs)
+  var fs = new RegExp(    sprintf('function %s[^}]+}[^}]+}', fcnm.replace('$', '\\$'))  );//alert("fs="+fs)
   //var fs = new RegExp('function ' + fcnm.replace('\$','\\$') + '[^}]+}[^}]+}');
 
   function fcobj(){
-    var mch = rpt.match(fs)[0]; alert("rpt.match(fs)[0]="+mch); mch = mch.split('');alert(typeof mch)
+    var mch = rpt.match(fs)[0]; mch = mch.split('')
     for (j=0;j<mch.length;j++) {
       if (mch[j] === "$") {
         mch[j]="\\$"
       }
     };
     var mch = mch.join('');
-    var mch = mch.split('\;'); //alert(mch[4])
+    var mch = mch.split('\;'); alert(mch[4])
     for (i=0;i<mch.length;i++) {
       var zzx = mch[i].substring(0,3);
       if ((zzx === zzy) && (zzx.charAt(2)==='.')) { var zzz = zzy.substring(0,2) };
@@ -210,10 +210,10 @@ var proxies = ['https://allow-any-origin.appspot.com/https:','https://cors-anywh
         var zzy = zzx
       }
     }
-    var mch = new RegExp('var ' + zzz + '[^}]+}[^}]+}[^}]+}};');
+    var mch = new RegExp('var ' + zzz + '[^}]+}[^}]+}[^}]+}};');alert(mch + "  " + zzz)
     return [mch,zzz]
   }
-  alert("rpt.match(fcobj()[0])="+rpt.match(fcobj()[0])+" ; fcobj()[1]="+fcobj()[1]+" ; rpt.match(fs)[0]="+rpt.match(fs)[0])
+  alert("rpt.match(fcobj()[0])="+rpt.match(fcobj()[0]));//+" ; fcobj()[1]="+fcobj()[1]+" ; rpt.match(fs)[0]="+rpt.match(fs)[0])
   eval(rpt.match(fcobj()[0])[0].split(" " + fcobj()[1] + "=").join(" dekrypt0=") + rpt.match(fs)[0].split(fcobj()[1]).join("dekrypt0"));
   //eval(rpt.match(fcobj()[0])[0].replace(fcobj()[1],"dekrypt0") + rpt.match(fs)[0].split(fcobj()[1]).join("dekrypt0"));
 
