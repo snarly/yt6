b6db3af5feacdc3442ae45922457ff65337a76b4
snarly <snarly@users.noreply.github.com>
2015-07-31 12:19:52 +0200

    Update yt6.js

--- yt6.js
+++ yt6.js
@@ -2169,7 +2169,7 @@ if ( getElementsByAttribute(document,'div','class','yt-masthead-logo-container')
 
 mep_up()
             };
-            loadScript( protocol() + "//cdn.rawgit.com/snarly/yt6/05010e630f4e5126913be2b73d15b324bae46678/mep-feature-speed.js",jq5)
+            loadScript( protocol() + "//cdn.rawgit.com/snarly/yt6/c882899f4b8f66c53526cb09ce780efff0bfa3f1/mep-feature-speed.js",jq5)
 //		    loadScript( protocol() + "//cdn.rawgit.com/snarly/yt6/feeba89eb6793a26476cab7c985365592c80dfd9/mep-feature-speed.js",jq5);
 //          };
 //          loadScript( protocol() + "//cdn.rawgit.com/snarly/yt6/f684d6c92fc170be45129a7f2c12069689fcc0b0/mep-feature-playlist.js",jq4)
