b3c5460d6d2a7659afbd44a78d2c2b5176f437c3
snarly <snarly@users.noreply.github.com>
2015-08-01 15:03:20 +0200

    Update mep-feature-speed.js

--- mep-feature-speed.js
+++ mep-feature-speed.js
@@ -87,10 +87,11 @@
 			.height(
 				speedButton.find('.mejs-speed-selector ul').outerHeight(true) + 
 				speedButton.find('.mejs-speed-translations').outerHeight(true))
-			.css('top', ( -1 * ( speedSelector.height() + 81 ) ) + 'px');
 
-		document.getElementsByClassName('mejs-speed-selector')[0]
-			.csstop = ( -1 * ( speedSelector.height() + 81 ) ) + 'px';
+	//		.css('top', ( -1 * ( speedSelector.height() + 81 ) ) + 'px');
+
+	//	document.getElementsByClassName('mejs-speed-selector')[0]
+	//		.csstop = ( -1 * ( speedSelector.height() + 81 ) ) + 'px';
 
 
 //            var displaySpeed =
