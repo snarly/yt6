41bc49bc9a194938c0b5010a600b9e8dfb8759d9
snarly <snarly@users.noreply.github.com>
2015-08-02 17:13:21 +0200

    Update yt0.js

--- javascripts/yt0.js
+++ javascripts/yt0.js
@@ -3349,7 +3349,7 @@ if ( (typeof player().getPlayerState == 'function') && (player().getAttribute('f
 
 	  if (webgl) {
 	    webgl.parentNode.style.left = -1 * parseInt(webgl.parentNode.parentNode.style.left.replace('px','')) + parseInt(v.style.left.replace('pc','')) + 'px'
-	    webgl.parentNode.style.top = -1 * parseInt(webgl.parentNode.parentNode.style.top.replace('px','')) + parseInt(v.style.left.replace('px','')) + 'px'
+	    webgl.parentNode.style.top = -1 * parseInt(webgl.parentNode.parentNode.style.top.replace('px','')) + parseInt(v.style.top.replace('px','')) + 'px'
 	  }
   }
 
@@ -3540,7 +3540,7 @@ if ((v != stage) || ((v.getAttribute('name') != null) && (v.getAttribute('name')
 
 	  if (webgl) {
 	    webgl.parentNode.style.left = -1 * parseInt(webgl.parentNode.parentNode.style.left.replace('px','')) + parseInt(v.style.left.replace('pc','')) + 'px'
-	    webgl.parentNode.style.top = -1 * parseInt(webgl.parentNode.parentNode.style.top.replace('px','')) + parseInt(v.style.left.replace('px','')) + 'px'
+	    webgl.parentNode.style.top = -1 * parseInt(webgl.parentNode.parentNode.style.top.replace('px','')) + parseInt(v.style.top.replace('px','')) + 'px'
 	    webgl.style[prop] = v.style[prop]
 	  }
 
