7bf9bf73aedec1c3ed66da96c8e013466e763152
snarly <snarly@users.noreply.github.com>
2015-08-12 21:21:05 +0200

    Update yt0.js

--- javascripts/yt0.js
+++ javascripts/yt0.js
@@ -2913,8 +2913,8 @@ console.log('4: ' + p1.style.left + p1.style.width)
 
 
       } else {
-alert(document.getElementById('snarls_player').size)
-	if (document.getElementById('snarls_player').size != 'defaultr'){
+
+	if (document.getElementById('snarls_player').size == 'theater'){
 
 	  if ( (me_aspect != false) && (document.getElementById('aspect')) ) { 
 	    w = windowwidth
