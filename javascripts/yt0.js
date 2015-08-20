51bdbf9196a4ece9982f97c8885e211ebaddcb36
snarly <snarly@users.noreply.github.com>
2015-08-21 00:46:38 +0200

    Update yt0.js

--- javascripts/yt0.js
+++ javascripts/yt0.js
@@ -2587,7 +2587,23 @@ if ((yt6 != null) && (yt6.loaded)) {
 
 	}//mep_reload
 
-
+	      if ((bm0 == null) && (p != null) && (p.getAttribute('class') != null)) {
+		/*var z = document.getElementById('player-api').textContent.split(',"title":"')[1]
+		console.log('bm0' + z);
+		if (z == undefined) {
+		  var z = yt6.strLocation.split('&')[0];console.log(z)
+		  xhr.open('get', z, false);
+  		  xhr.send('');
+  		  var z = parseXml(xhr.responseText);
+  		  var ads = z.getElementById('player-api').textContent.split(',"title":"')[1]
+		  console.log('bm0' + ads);
+		}
+		if ( (typeof window.ytplayer != 'object') || ((typeof window.ytplayer == 'object') && (window.ytplayer.config == null)) ) {
+		  window.ytplayer = {};
+		  eval(z.getElementById('player-api').textContent.split('var ytplayer = ytplayer || {};')[1].split(';(function()')[0]);
+		}*/
+		mep_reload()
+	      }
 
 
   var z = p.getAttribute('class');
@@ -2744,23 +2760,7 @@ if ((yt6 != null) && (yt6.loaded)) {
 	    }//else
 
 	  } /*else {//args
-	      if ((bm0 == null) && (p != null) && (p.getAttribute('class') != null)) {
-		var z = document.getElementById('player-api').textContent.split(',"title":"')[1]
-		console.log('bm0' + z);
-		if (z == undefined) {
-		  var z = yt6.strLocation.split('&')[0];console.log(z)
-		  xhr.open('get', z, false);
-  		  xhr.send('');
-  		  var z = parseXml(xhr.responseText);
-  		  var ads = z.getElementById('player-api').textContent.split(',"title":"')[1]
-		  console.log('bm0' + ads);
-		}
-		if ( (typeof window.ytplayer != 'object') || ((typeof window.ytplayer == 'object') && (window.ytplayer.config == null)) ) {
-		  window.ytplayer = {};
-		  eval(z.getElementById('player-api').textContent.split('var ytplayer = ytplayer || {};')[1].split(';(function()')[0]);
-		}
-		mep_reload()
-	      }
+
 	    }*/
 
 	}//watch
