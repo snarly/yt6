1d023dedcd14f12096562abff1c1f7edcfd29fff
snarly <snarly@users.noreply.github.com>
2015-09-17 06:17:16 +0200

    Update yt0.js

--- javascripts/yt0.js
+++ javascripts/yt0.js
@@ -1166,6 +1166,22 @@ if ((typeof autoplay == 'string') || (typeof autoplay2[0] != 'undefined')) { ret
 }
 
 
+
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseFloat(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+    var seconds = seconds.toFixed(3);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
+
 function crossXmlHttpReq(ytplayer){
 
 
@@ -3080,20 +3096,6 @@ window.ytplayer.config.loaded = true;
 
 
 
-
-String.prototype.toHHMMSS = function () {
-    var sec_num = parseFloat(this, 10); // don't forget the second param
-    var hours   = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
-    var seconds = seconds.toFixed(3);
-
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+':'+minutes+':'+seconds;
-    return time;
-}
 
 function getScrollbarWidth() {
     var outer = document.createElement("div");
