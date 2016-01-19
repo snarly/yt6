6a354db2afe975f03122ea694d77e4f660f98851
snarly <snarly@users.noreply.github.com>
2016-01-19 19:46:00 +0100

    Update yt0.js

--- javascripts/yt0.js
+++ javascripts/yt0.js
@@ -3363,12 +3363,12 @@ if ( (me_aspect) && (  (document.getElementById(mep_x('mep_')) == null) || ((pla
 	  p1.style.width = Math.round((h - hdiff) * document.getElementById('snarls_player').aspect_ratio) + 'px';
 	  p1.style.height = Math.round(h - hdiff) + 'px';
 	  p1.style.left = Math.round( (w - p1.style.width.replace('px','')) / 2) + 'px';
-console.log('2: ' + p1.style.left + p1.style.width);
+//console.log('2: ' + p1.style.left + p1.style.width);
 	} else {
 	    p1.style.width = w + 'px'
 	    p1.style.height = h + 'px'
 	    p1.style.left = '0px';
-console.log('3: ' + p1.style.left + p1.style.width + p1.style.height);
+//console.log('3: ' + p1.style.left + p1.style.width + p1.style.height);
 	  }
       }
 	p1.style.top = '0px';
@@ -3377,12 +3377,12 @@ console.log('3: ' + p1.style.left + p1.style.width + p1.style.height);
   if ( (w < (1 * p1.style.width.replace('px',''))) || ( (windowheight < (1 * p1.style.height.replace('px',''))) && (document.getElementById('snarls_player').size == 'theater') ) ) {
 
     if ( ((1 * p1.style.height.replace('px','')) < windowheight ) || (w < (1 * p1.style.width.replace('px',''))) ){//|| (!((1 * p1.style.height.replace('px','')) > windowheight )) ) {
-console.log('3: ' + p1.style.left + p1.style.width);
+//console.log('3: ' + p1.style.left + p1.style.width);
 	    p1.style.width = w + 'px'
 	    p1.style.height = Math.round( w / document.getElementById('snarls_player').aspect_ratio ) - hdiff + 'px';
 	    p1.style.top = Math.round( (h - p1.style.height.replace('px','')) / 2 - (hdiff / 2) ) + 'px';
 	    p1.style.left = Math.round( (w - (p1.style.height.replace('px','') * document.getElementById('snarls_player').aspect_ratio)) / 2) + 'px';
-console.log('4: ' + p1.style.left + p1.style.width + p1.style.height);
+//console.log('4: ' + p1.style.left + p1.style.width + p1.style.height);
 
 
       } else {
@@ -3400,14 +3400,14 @@ console.log('4: ' + p1.style.left + p1.style.width + p1.style.height);
 		p1.style.height = h + 'px'
 		p1.style.left = '0px';
 		p1.style.top = '0px'
-console.log('5: ' + p1.style.left + p1.style.height + p1.style.height);
+//console.log('5: ' + p1.style.left + p1.style.height + p1.style.height);
 	      }
 	  } else {
 	      p1.style.width = (h - hdiff) * document.getElementById('snarls_player').aspect_ratio + 'px';
 	      p1.style.height = h - hdiff + 'px';
 	      p1.style.top = (h - p1.style.height.replace('px','')) / 2 - (hdiff / 2) + 'px';
 	      p1.style.left = (w - p1.style.width.replace('px','')) / 2 + 'px';
-console.log('6: ' + p1.style.left + p1.style.height + p1.style.height);
+//console.log('6: ' + p1.style.left + p1.style.height + p1.style.height);
 	    }
 
 		w = w + 'px'
@@ -3438,7 +3438,7 @@ console.log('6: ' + p1.style.left + p1.style.height + p1.style.height);
 		  document.getElementById('a_width').value = w
 		  document.getElementById('a_height').value = h
 		}
-console.log('7: ' + p1.style.left + p1.style.height + p1.style.height);
+//console.log('7: ' + p1.style.left + p1.style.height + p1.style.height);
 
 	}//theater
 
@@ -3453,7 +3453,7 @@ console.log('7: ' + p1.style.left + p1.style.height + p1.style.height);
 	  (!(Math.abs(document.getElementById('player1').width - p1.style.width.replace('px','')) > 2) && !(Math.abs(document.getElementById('player1').height - p1.style.height.replace('px','')) > 2))
 	)
       )
-    {console.log('rescale')
+    {//console.log('rescale')
 	p1.style.width = document.getElementById('player1').width + 'px'
 	p1.style.height = document.getElementById('player1').height + 'px'
 //      if (Math.abs(p1.style.width.replace('px','') - Math.floor(p1.style.width.replace('px','') / 2) * 2) < Math.abs(p1.style.width.replace('px','') - Math.ceil(p1.style.width.replace('px','') / 2) * 2)) { p1.style.width = Math.floor(p1.style.width.replace('px','') / 2) * 2 + 2 + 'px' } else { p1.style.width = Math.ceil(p1.style.width.replace('px','') / 2) * 2 + 'px' }
