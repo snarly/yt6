1066a417f30bd1902a5ba6b3ab22520c683242f9
snarly <snarly@users.noreply.github.com>
2016-10-24 08:09:02 +0200

    Update yt6.js

--- yt6.js
+++ yt6.js
@@ -1577,7 +1577,7 @@ if (autoplay != null) {
 	if (btn) {
 	  var b = ';var yt6 = document.getElementById("snarls_player"); if (yt6) { if (this.getAttribute("class").indexOf("yt-uix-button-toggled") != -1) { this.setAttribute("class", this.getAttribute("class").split(" yt6").join("") + " yt6"); yt6.autoplay = true } else { this.setAttribute("class", this.getAttribute("class").split(" yt6").join("")); yt6.autoplay = false } }; return false;'
 	  btn.setAttribute('onclick', b);
-	]
+	}
         if (autoplay.parentNode.innerHTML.split('</button>')[0].indexOf('yt-uix-button-toggled') != -1) {
 	  yt6.autoplay = true
 	} else {
