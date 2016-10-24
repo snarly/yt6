789599ef60cfb52dee9c0e91575fa5545cb5f056
snarly <snarly@users.noreply.github.com>
2016-10-24 09:22:10 +0200

    Update yt6.js

--- yt6.js
+++ yt6.js
@@ -1573,22 +1573,17 @@ if (autoplay != null) {
     var autoplay = document.getElementsByClassName('playlist-nav-controls')[0] || document.getElementsByClassName('appbar-playlist-controls clearfix')[0]
     if (autoplay != undefined) {
       if (autoplay.getAttribute('class') == 'playlist-nav-controls') {
-        var btn = autoplay.getElementsByTagName('button')[0];
-	if (btn) {
-	  var b = ';var yt6 = document.getElementById("snarls_player"); if (yt6) { if (this.getAttribute("class").indexOf("yt-uix-button-toggled") == -1) { yt6.autoplay = true } else { yt6.autoplay = false } }; return false;'
-	  btn.setAttribute('onclick', b);
-	}
-        if (autoplay.parentNode.innerHTML.split('</button>')[0].indexOf('yt-uix-button-toggled') != -1) {
+	if (autoplay.parentNode.innerHTML.split('</button>')[0].indexOf('yt-uix-button-toggled') != -1) {
 	  yt6.autoplay = true
 	} else {
 	    if (yt6.autoplay) {//"Replay playlist" sometimes greyed out by YT
-	      if (start) { //if (btn) btn.click()
-		/*var btn = autoplay.getElementsByTagName('button')
+	      if (start) {
+		var btn = autoplay.getElementsByTagName('button')
 		for (var i in btn){
 		  if (btn[i] && yt6.autoplay == true) {
 		    btn[i].click(); break
 		  }
-		}*/
+		}
 	      } else {
 		  yt6.autoplay = false;
 		}
