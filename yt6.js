b34a9a536fbf0a5e0b260700089cb26ae8ba7cc4
snarly <snarly@users.noreply.github.com>
2017-05-22 10:02:13 +0200

    Update yt6.js

--- yt6.js
+++ yt6.js
@@ -6484,10 +6484,10 @@ function mep_run() {
 					          //if (yt6.x) {
 					            Seek = 1;
 						    yt6.player1.pause();
-						    if (!(me.loaded && player1.media.loaded) && yt6.player1.media.currentTime < 1) {
+						    if (!(me.loaded && player1.media.loaded)) {
 						      $waitUntil(function(){if(me.loaded && player1.media.loaded) return true},
-						        function(){ yt6.player1.media.currentTime = me.currentTime },400,1200)
-						    } //else if(yt6.player1.media.currentTime != me.currentTime) yt6.player1.media.currentTime = me.currentTime;
+						        function(){ yt6.player1.media.currentTime = me.currentTime },400,400)
+						    } else yt6.player1.media.currentTime = me.currentTime;
 					          //}
 					        }
 					    } else me.pause()
