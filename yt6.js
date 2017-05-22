e6be0ad1245dd5b52b3cc05d463b3a770b76812c
snarly <snarly@users.noreply.github.com>
2017-05-22 10:28:29 +0200

    Update yt6.js

--- yt6.js
+++ yt6.js
@@ -6482,12 +6482,11 @@ function mep_run() {
 					        player1.play(); //prevents an unwanted video restart
 					      } else {
 					          //if (yt6.x) {
-					            Seek = 1;
-						    yt6.player1.pause();
-						    if (!(me.loaded && player1.media.loaded)) {
-						      $waitUntil(function(){if(me.loaded && player1.media.loaded) return true},
-						        function(){ yt6.player1.media.currentTime = me.currentTime },400,400)
-						    } else yt6.player1.media.currentTime = me.currentTime;
+						    if (!yt6.tilt) {//(me.loaded && player1.media.loaded)) {
+						      $waitUntil(function(){if (me.loaded && !yt6.player1.media.paused) { return true } else { yt6.tilt = 1; yt6.Seek = 1; yt6.player1.pause(); yt6.player1.media.currentTime = me.currentTime }},
+						        function(){ delete yt6.tilt },400,400*2*yt6.speed)
+						      
+						    }
 					          //}
 					        }
 					    } else me.pause()
