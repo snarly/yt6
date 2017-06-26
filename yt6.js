d4f8fe84270c43fe31163cc734bd6d8c94622756
snarly <snarly@users.noreply.github.com>
2017-06-26 07:18:45 +0200

    Update yt6.js

--- yt6.js
+++ yt6.js
@@ -7713,7 +7713,7 @@ if (yt6.ytg) {
 	  a = a.style
 	  a.top = -1 * yt6.ytg.style.top.replace('px','') + yt6.mhp.offsetHeight + 20 + 'px'
 	  var sb = yt6.wsb = document.getElementById('sidebar') || document.getElementsByTagName('ytg-watch-sidebar')[0]
-	  if ( ( yt6.size && yt6.size != 'default' ) || ( !yt6.wsb.getAttribute('id') && ( yt6.size == 'default' || yt6.ww > 1 * yt6.w.replace('px','') )) ) {
+	  if ( ( yt6.size && yt6.size != 'default' ) || ( yt6.wsb && !yt6.wsb.getAttribute('id') && ( yt6.size == 'default' || yt6.ww > 1 * yt6.w.replace('px','') )) ) {
 	    if (yt6.ww >= 1 * a.width.replace('px','')) {
 	      a.left = (1 * yt6.ww - 1 * a.width.replace('px','')) / 2 - yt6.ytg.style.left.replace('px','') + 'px'
 	    } else {
