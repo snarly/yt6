4d2a05efe382e0910026f8185b0a7d085eaede6e
snarly <snarly@users.noreply.github.com>
2018-07-01 05:30:41 +0200

    Update yt6.js

--- yt6.js
+++ yt6.js
@@ -8396,7 +8396,7 @@ function mep_run() {
 						          yt6.navigation = true
 												//mp.playVideoAt(yt6.pl_index+1) || mp.nextVideo()
 						          //function nextOn() { if (!yt6.pre_ad) { var a = mp.getCurrentTime(); mp.seekTo(mp.getDuration()); mp.stopping = false; FireEvent2(mp, 'mouseup'); mp.playVideo(); if (mp.getCurrentTime() == a) { } else { zi.click() }; if (yt6.pl_index < mp.getPlaylistIndex()) { zi.click() } else if (mp != yt6.movie_player) { pl_item(); yt6.pl_next.click() } } else { pl_item(); yt6.pl_next.click() } }
-						          function nextOn() { if (!yt6.pre_ad) { var a = mp.getCurrentTime(); mp.seekTo(mp.getDuration()); mp.stopping = false; FireEvent2(mp, 'mouseup'); mp.playVideo(); if (mp.getCurrentTime() == a || mp.getCurrentTime() < mp.getDuration()) { zi.click() } else { if (yt6.pl_index < mp.getPlaylistIndex()) { zi.click() } else if (mp != yt6.movie_player) { pl_item(); yt6.pl_next.click() } else { zi.click() } } else { pl_item(); yt6.pl_next.click() }; mp.pauseVideo() }
+						          function nextOn() { if (!yt6.pre_ad) { var a = mp.getCurrentTime(); mp.seekTo(mp.getDuration()); mp.stopping = false; FireEvent2(mp, 'mouseup'); mp.playVideo(); if (mp.getCurrentTime() == a || mp.getCurrentTime() < mp.getDuration()) { zi.click() } else { if (yt6.pl_index < mp.getPlaylistIndex()) { zi.click() } else if (mp != yt6.movie_player) { pl_item(); yt6.pl_next.click() } else { zi.click() } } } else { pl_item(); yt6.pl_next.click() }; mp.pauseVideo() }
 
 						          //if (!yt6.ytg) {
 							    if (!yt6.shuffle) {
@@ -8622,7 +8622,7 @@ function mep_run() {
 						          yt6.navigation = true
 												//mp.playVideoAt(yt6.pl_index+1) || mp.nextVideo()
 						          //function nextOn() { if (!yt6.pre_ad) { var a = mp.getCurrentTime(); mp.seekTo(mp.getDuration()); mp.stopping = false; FireEvent2(mp, 'mouseup'); mp.playVideo(); if (mp.getCurrentTime() == a) { } else { zi.click() }; if (yt6.pl_index < mp.getPlaylistIndex()) { zi.click() } else if (mp != yt6.movie_player) { pl_item(); yt6.pl_next.click() } } else { pl_item(); yt6.pl_next.click() } }
-						          function nextOn() { if (!yt6.pre_ad) { var a = mp.getCurrentTime(); mp.seekTo(mp.getDuration()); mp.stopping = false; FireEvent2(mp, 'mouseup'); mp.playVideo(); if (mp.getCurrentTime() == a || mp.getCurrentTime() < mp.getDuration()) { zi.click() } else { if (yt6.pl_index < mp.getPlaylistIndex()) { zi.click() } else if (mp != yt6.movie_player) { pl_item(); yt6.pl_next.click() } else { zi.click() } } else { pl_item(); yt6.pl_next.click() }; mp.pauseVideo() }
+						          function nextOn() { if (!yt6.pre_ad) { var a = mp.getCurrentTime(); mp.seekTo(mp.getDuration()); mp.stopping = false; FireEvent2(mp, 'mouseup'); mp.playVideo(); if (mp.getCurrentTime() == a || mp.getCurrentTime() < mp.getDuration()) { zi.click() } else { if (yt6.pl_index < mp.getPlaylistIndex()) { zi.click() } else if (mp != yt6.movie_player) { pl_item(); yt6.pl_next.click() } else { zi.click() } } } else { pl_item(); yt6.pl_next.click() }; mp.pauseVideo() }
 
 						          //if (!yt6.ytg) {
 							    if (!yt6.shuffle) {
