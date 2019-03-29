2fb98167e98405be8cb99ff18c6adf6acbe8d120
snarly <snarly@users.noreply.github.com>
2019-03-29 16:43:08 +0100

    Update yt0.js

--- javascripts/yt0.js
+++ javascripts/yt0.js
@@ -9122,7 +9122,7 @@ function mep_run() {
 					    me.p = null
 					    if (yt6.x && me.currentTime.toFixed(2) != player1.media.currentTime.toFixed(2)) {// && (1*yt6.retry - 0) < 8
 					    try { player1.media.currentTime = me.currentTime; //Seek = 1;
-					    } catch(e) { console.log('yt6.player2.media.currentTime = ' + me.currentTime); player1.media.setCurrentTime( me.currentTime ) }
+					    } catch(e) { console.log('yt6.player2.media.currentTime = ' + me.currentTime); }
 					      if (!yt6.Seeked2) {console.log('2s1 '+ yt6.newvideo)
 						if (yt6.diff > parseFloat(0.3)) {
 						  Seek = 1; me.pause(); //gc('rotateleft snarl-button yt-uix-button-text')[0].innerHTML = yt6.diff
