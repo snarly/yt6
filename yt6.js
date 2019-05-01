a38a82a9bc057f6452dc3415f4c48005ffcf1587
snarly <snarly@users.noreply.github.com>
2019-05-01 03:48:50 +0200

    Update yt6.js

--- yt6.js
+++ yt6.js
@@ -933,7 +933,7 @@ function swfbin4(){
     }
   if (yt6.swfbin4) {
     yt6.swfbin4 = yt6.swfbin4.split('"url":"')[1]
-    if (yt6.swfbin4) yt6.swfbin4 = yt6.swfbin.split('"')[0]
+    if (yt6.swfbin4) yt6.swfbin4 = yt6.swfbin4.split('"')[0]
     if (yt6.swfbin4) {
 	yt6.swfbin4 = yt6.swfbin4.split('player-')[1].split('\\')[0]
 	yt6.swfbin4 = 'https://s.ytimg.com/yts/swfbin/player-' + yt6.swfbin4 + '/watch_as3.swf'
